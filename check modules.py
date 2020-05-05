7fd7bbdcd7f812710dc3a603cca224de97ca5f89
DNDR <62344549+DNDR21@users.noreply.github.com>
2020-05-05 11:54:57 +0300

    Update check modules.py

--- check modules.py
+++ check modules.py
@@ -1,6 +1,6 @@
-import os
+import os#import os lib
 
-modules=["setuptools","kivy","requests","bs4"]
+modules=["setuptools","kivy","requests","bs4"]#modules to be added
 
 for i in modules:
-	os.system("pip install "+i)
+	os.system("pip install "+i)#download modules
