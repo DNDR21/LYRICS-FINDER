0c8ce9e6454427f53b09cd55d975b95595fcda39
DNDR <62344549+DNDR21@users.noreply.github.com>
2020-06-23 13:49:47 +0300

    Update check modules.py

--- check modules.py
+++ check modules.py
@@ -1,6 +1,6 @@
 import os#import os lib
 
-install_modules=["setuptools","--upgrade pip wheel setuptools virtualenv","docutils pygments pypiwin32 kivy_deps.sdl2==0.1.* kivy_deps.glew==0.1.*","kivy_deps.gstreamer==0.1.*","kivy_deps.angle==0.1.*","kivy==1.11.1","requests","bs4"]#modules to be added
+install_modules=["setuptools","--upgrade pip wheel setuptools virtualenv","docutils pygments pypiwin32 kivy_deps.sdl2==0.1.* kivy_deps.glew==0.1.*","kivy_deps.gstreamer==0.1.*","kivy_deps.angle==0.1.*","kivy","requests","bs4"]#modules to be added
 
 uninstall_modules=["-y kivy.deps.glew kivy.deps.gstreamer kivy.deps.sdl2 kivy.deps.angle"]
 
